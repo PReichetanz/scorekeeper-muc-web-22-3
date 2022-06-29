0cc3915a47b901f7753550029d9f3e61d5823cdb
PReichetanz <paul.reichetanz@gmx.de>
2022-06-29 12:54:46 +0200

    Add Player and Button component to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import Player from "./components/Player/Player.js";
+import Button from "./components/Button/Button.js";
+
+const players = [
+  {
+    name: "John",
+    score: 15,
+  },
+  {
+    name: "Jane",
+    score: 15,
+  },
+  {
+    name: "Jim",
+    score: 15,
+  },
+];
+
 function App() {
-  return <h1>Scorekeeper</h1>;
+  return (
+    <>
+      <heading>
+        <h1>Game</h1>
+      </heading>
+      <main>
+        {players.map((player) => (
+          <Player name={player.name} score={player.score} />
+        ))}
+        <Button background={"red"}>End game</Button>
+      </main>
+    </>
+  );
 }
 
 export default App;
